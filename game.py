db25fa54253b48910d3de1efc91c0b5c36db8370
Aidai Nuralieva <nuralievaaidai1103@gmail.com>
2025-01-30 19:44:30 +0600

    random

---
+++ game.py
@@ -0,0 +1,21 @@
+import random 
+print("Угадайте число от 1 до 100, удачи!")
+
+number = random.randint(1,100)
+attempts=0
+
+while True:
+    guess=int(input("Ваш вариант:"))
+    attempts +=1
+    difference = abs(number-guess)
+
+    if guess==number:
+        print("Поздравляю! вы угадали число! ")
+    elif difference>30:
+        print("Холодно!")
+    elif difference>20:
+        print("Прохладно")
+    elif difference>10:
+        print("Тепло")
+    else:
+        print("Жарко")
